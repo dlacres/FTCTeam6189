4b59b35ad8ea1f2def11259ca4ce46a762befde9
abowen <2000andrewbowen@gmail.com>
2014-11-27 20:12:47 -0600

    Work on Auto

---
+++ sw2014_2015/i_IrReading.c
@@ -0,0 +1,18 @@
+
+void IrReading(bool Ir(int ir_position))
+	{
+		while(true)
+		{
+			if(SensorValue[IR]==ir_position)
+			{
+			Ir=true;
+			}
+
+			else
+			{
+			Ir=false;
+			}
+		{
+
+		return;
+	}

8cc6557378c9e7f2ebe08244b3ca0762ec7a15ac
unknown <dlacres@gmail.com>
2015-08-06 13:13:38 -0500

    Use Encoder

--- SummerCamp/SummerCamp/Includes/i.left.c
+++ SummerCamp/SummerCamp/Includes/i.left.c
@@ -1,32 +1,16 @@
 void Left (int speed, int Degrees)
 {
-	int wait;
+	nMotorEncoder[rightmotor] = 0;
+	nMotorEncoder[leftmotor] = 0;
 
-switch (speed)
-{
-case 100:
-  wait=Degrees*4;
-  break;
+	//250 counts per motor revolution = 6.9 inches (Wheel diameter is 2.2 inches)
+	while ((abs(nMotorEncoder[rightmotor])+abs(nMotorEncoder[leftmotor])) < (distance*72))  // wait for motor to reach a specific location
+	{
+  	motor[rightmotor] = speed;
+  	motor[leftmotor] = -speed;
+	}
 
-case 75:
-  wait=Degrees*4;
-  break;
+	motor[rightmotor]=0;
+	motor[leftmotor]=0;
 
-case 50:
-  wait=Degrees*5;
-  break;
-
-case 25:
-  wait=Degrees*7;
-  break;
-
-default:
-  break;
-}
-
-motor[rightmotor]=speed;
-motor[leftmotor]=-speed;
-wait1Msec(wait);
-motor[rightmotor]=0;
-motor[leftmotor]=0;
 }
